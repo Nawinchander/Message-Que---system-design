6b94c03dae62aadd15b3d189a6562b564d8990bb
Nawin chander <nawinnapzy289@gmail.com>
2026-04-15 21:09:30 +0530

    ad

--- Faang/Pub sub pattern/Notification.js
+++ Faang/Pub sub pattern/Notification.js
@@ -4,3 +4,27 @@
 // Used in microservices
 
 
+class PubSub {
+  constructor() {
+    this.subscribers = [];
+  }
+
+  subscribe(fn) {
+    this.subscribers.push(fn);
+  }
+
+  publish(msg) {
+    this.subscribers.forEach(fn => fn(msg));
+  }
+}
+
+const pubsub = new PubSub();
+
+// Subscribers
+pubsub.subscribe(msg => console.log("Email:", msg));
+pubsub.subscribe(msg => console.log("SMS:", msg));
+
+// Publisher
+pubsub.publish("New Order Created");
+
+
